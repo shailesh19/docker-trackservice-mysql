6c3fec93c813108a8fb706f058627da5fdac0012
shailesh19 <beingshailesh19@gmail.com>
2019-08-06 09:37:28 +0530

    Lombok added

--- src/main/java/com/stackroute/trackservice/domain/Track.java
+++ src/main/java/com/stackroute/trackservice/domain/Track.java
@@ -1,67 +1,23 @@
 package com.stackroute.trackservice.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import java.util.List;
 
 @Entity
+@NoArgsConstructor
+// creates default constructor
+@AllArgsConstructor
+// creates parameterized constructor with all properties
+@Data
 public class Track
 {
     @Id
     private int trackId;
     private String trackName;
     private String comments;
-
-    public Track()
-    {
-    }
-
-    public Track(int trackId, String trackName, String comments)
-    {
-        this.trackId = trackId;
-        this.trackName = trackName;
-        this.comments = comments;
-    }
-
-    public int getTrackId()
-    {
-        return trackId;
-    }
-
-    public void setTrackId(int trackId)
-    {
-        this.trackId = trackId;
-    }
-
-    public String getTrackName()
-    {
-        return trackName;
-    }
-
-    public void setTrackName(String trackName)
-    {
-        this.trackName = trackName;
-    }
-
-    public String getComments()
-    {
-        return comments;
-    }
-
-    public void setComments(String comments)
-    {
-        this.comments = comments;
-    }
-
-    @Override
-    public String toString()
-    {
-        return "Track{" +
-                "trackId=" + trackId +
-                ", trackName='" + trackName + '\'' +
-                ", comments='" + comments + '\'' +
-                '}';
-    }
-
-
 }
